082f2a65572220cc4678ee4c0536f8f6e9fd961a
SilvanoSCF <sscf.prog@gmail.com>
2021-10-30 22:51:39 -0300

    Commit de introdução a estruturas de repetição e condição.

--- src/com/dio/Model/EstruturaDeRepeticaoERepeticao.java
+++ src/com/dio/Model/EstruturaDeRepeticaoERepeticao.java
@@ -8,7 +8,24 @@ public class EstruturaDeRepeticaoERepeticao {
 
     public static void main(String[] args) {
 
+        int numero = 2;
 
+        if (numero > 9){
+            System.out.println("Numero maior que 9");
+        }else{
+            System.out.println("Numero menor que 9");
+        }
+
+        switch (numero){
+            case 1:
+                System.out.println("Numero é um");
+                break;
+            case 2:
+                System.out.println("Numero é dois");
+                break;
+
+
+        }
 
 
 
