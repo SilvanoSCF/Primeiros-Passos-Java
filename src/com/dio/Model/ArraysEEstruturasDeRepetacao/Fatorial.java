b275f3c0d28f52198290c3d3e17563ede7e4016a
SilvanoSCF <sscf.prog@gmail.com>
2021-11-03 14:26:24 -0300

    Commit tabuada e fatorial

--- src/com/dio/Model/ArraysEEstruturasDeRepetacao/Fatorial.java
+++ src/com/dio/Model/ArraysEEstruturasDeRepetacao/Fatorial.java
@@ -1,8 +1,23 @@
 package com.dio.Model.ArraysEEstruturasDeRepetacao;
 
+import java.util.Scanner;
+
 public class Fatorial {
 
     // Ex 5! = 5 x 4 x 3 x 2 x 1 = 120  Fatorial
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int fatorial;
+        int multiplicacao = 1;
+        System.out.println("Fatorial :");
+        fatorial = scan.nextInt();
+
+        for (int i = fatorial; i > 1; i--)
+        {
+            multiplicacao = multiplicacao * i;
+        }
+        System.out.println(multiplicacao);
+    }
 
 
 }
