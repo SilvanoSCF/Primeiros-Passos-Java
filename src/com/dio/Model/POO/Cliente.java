c93666479ac5659a59a98155aaae69443093e0a4
SilvanoSCF <sscf.prog@gmail.com>
2021-11-20 09:45:16 -0300

    Commit - atualizações -19/11/2021

--- src/com/dio/Model/POO/Cliente.java
+++ src/com/dio/Model/POO/Cliente.java
@@ -1,6 +1,25 @@
 package com.dio.Model.POO;
 
-public class Cliente {
+import java.util.List;
 
+public class Cliente extends Pessoa{
+
+    public Integer codigo;
+    public String nome;
+    public Integer cpf;
+
+    private List<Endereco> enderecos;
+
+    public void adicionaEndereco (Endereco endereco)
+    {
+        if (endereco ==null)
+        {
+            throw new NullPointerException("Endereço não pode ser nulo !!!");
+        }
+        if (endereco.cep == null)
+        {
+            throw new NullPointerException("Cep não pode ser nulo !!!");
+        }
+    }
 
 }
