36c1306b216776d3902fdfcc31e87eda17aee550
SilvanoSCF <sscf.prog@gmail.com>
2021-11-23 00:34:05 -0300

    Commit - atualizações -23/11/2021

---
+++ src/com/dio/Model/DesafiosMatematicos/Matematicos.java
@@ -0,0 +1,26 @@
+package com.dio.Model.DesafiosMatematicos;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Matematicos {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());10
+
+        System.out.println("Informe o primeiro valor: ");
+        int a = Integer.parseInt(st.nextToken());
+        System.out.println("Informe o Segundo valor: ");
+        int b = Integer.parseInt(st.nextToken());
+        int total = a + b;
+
+        System.out.println("Total esperado é: ");
+        System.out.println("X = " + total);
+
+
+    }
+}
